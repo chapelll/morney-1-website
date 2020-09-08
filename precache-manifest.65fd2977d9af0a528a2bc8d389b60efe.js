b9916e8a9d36344e8889cd1c755542acb3db1339
chapelll <214426857@qq.com>
2020-09-08 10:35:01 +0800

    deploy

---
+++ precache-manifest.65fd2977d9af0a528a2bc8d389b60efe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4373c68d07440218b62",
+    "url": "/morney-1-website/css/app.999955bd.css"
+  },
+  {
+    "revision": "3f9ef29552923ec6673789af2cde377e",
+    "url": "/morney-1-website/index.html"
+  },
+  {
+    "revision": "b4373c68d07440218b62",
+    "url": "/morney-1-website/js/app.4058fa56.js"
+  },
+  {
+    "revision": "f01b40e2718c9be689a2",
+    "url": "/morney-1-website/js/chunk-vendors.97a6eff0.js"
+  },
+  {
+    "revision": "b0c72820654d086f2b526db8c6a20387",
+    "url": "/morney-1-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/morney-1-website/robots.txt"
+  }
+]);
